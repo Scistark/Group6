d5ce711d965338c8e9db7e442c4cb886286d5a82
eli schneckloth <eligarmodon@gmail.com>
2026-04-03 12:22:36 -0600

    added basic about and styles pages

---
+++ about.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>About Us</title>
+    <link rel="stylesheet" href="style.css"> 
+</head>
+<body>
+
+    <header>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="about.php">About Us</a>
+            <a href="color.php">Color Coordinate</a>
+        </nav>
+    </header>
+
+    <main>
+        <h1>About Our Team</h1>
+
+        <section class="team-member">
+            <h2>Eli Schneckloth</h2>
+            <img src="./resources/Eli.jpeg" alt="Your Avatar">
+            <p>
+                Hello, my name is Eli Schneckloth a 3rd year student at CSU studying computer science with a focus in cyber security.
+            </p>
+        </section>
+
+
+    </main>
+
+</body>
+</html>
