66e984959fd7f628e9959d8fc8c9f5a7d8d62f72
Nikolas-Augustini <snowkai@dorado.cs.colostate.edu>
2026-04-06 13:10:51 -0600

    Added page title.

--- color.php
+++ color.php
@@ -6,6 +6,21 @@
     <link rel="stylesheet" href="color.css"> 
 </head>
 <body>
+     <header class="nav-wrapper">
+        <nav class="navbar">
+            <div class="nav-left">
+                <img src="resources/image.png" class="logo">
+                <p class="tagline">Professional Color Coordination Tools</p>
+            </div>
 
+            <div class="nav-links">
+                <a href="index.php">Home</a>
+                <a href="about.php">About</a>
+                <a href="color.php">Color Coordinator</a>
+            </div>
+        </nav>
+    </header>
+
+    <h1>Color Coordinator</h1>
 </body>
 </html>
