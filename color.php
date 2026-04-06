762d0ea3c25f7e4a0bd6ea2f708d743654e16868
Chimerabot <gideon@lo-fi.net>
2026-04-06 17:23:38 -0600

    Move coordinate grid to fragment folder

--- color.php
+++ color.php
@@ -67,27 +67,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
     echo "</table>";
 
-    echo "<h2>Coordinate Grid</h2>";
-
-    echo "<table class='grid-table'>";
-    for ($r = 0; $r <= $rows; $r++) {
-        echo "<tr>";
-        for ($c = 0; $c <= $rows; $c++) {
-            
-            if ($r == 0 && $c == 0) {
-                echo "<th></th>";
-            } elseif ($r == 0) {
-                echo "<th>" . chr(64 + $c) . "</th>";
-            } elseif ($c == 0) {
-                echo "<th>$r</th>";
-            } else {
-                echo "<td></td>";
-            }
-            
-        }
-        echo "</tr>";
-    }
-    echo "</table>";
+    include 'fragments/coordinateGrid.php';
 }
 }
 ?>
